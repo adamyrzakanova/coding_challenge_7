a9854f59879e20040847d6d30768bec9cf0a009b
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-16 23:34:28 -0500

    Task 3 - Customer Loyalty Discount.

--- cc_7.js
+++ cc_7.js
@@ -22,3 +22,19 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {
 // console log
 console.log(calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
 console.log(calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
+
+
+// Task 3
+
+const calculateLoyaltyDiscount = (amount, years) => {
+    let discountRate = 0.05;
+    if (years >= 5) discountRate = 0.15;
+    else if (years >= 3) discountRate = 0.10;
+
+    const discountedPrice = amount * (1 - discountRate);
+    return `Discounted Price: $${discountedPrice.toFixed(2)}`;
+  };
+
+// console log
+console.log(calculateLoyaltyDiscount(100, 6)); // Expected output: "Discounted Price: $85.00"
+console.log(calculateLoyaltyDiscount(200, 2)); // Expected output: "Discounted Price: $190.00"
