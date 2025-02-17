a23692c405f1c95950d19529a56cef39fc3c9b1c
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-16 23:33:36 -0500

    Task 2 - Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -10,3 +10,15 @@ function calculateInvoice(amount, TaxRate, discount) {
 // console log 
 console.log(calculateInvoice(100, 0.08, 5)); // Expected output: "Total Invoice: $103.00"
 console.log(calculateInvoice(500, 0.1, 20)); // Expected output: "Total Invoice: $530.00"
+
+
+// Task 2
+
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    const hourlyWage = salary / (hoursPerWeek * 52);
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
+  };
+
+// console log
+console.log(calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
+console.log(calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
